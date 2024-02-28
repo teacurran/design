69894ef1a078d40c8eb5f51ea4d5783ab6cb2e74
Terrence Curran <tcurran@approachingpi.com>
2024-02-28 10:42:46 -0500

    changed security group for load balancer

--- calendar/infrastructure/load_balancer.tf
+++ calendar/infrastructure/load_balancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "calendar" {
   name               = replace(local.domain_name, ".", "-")
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_traffic.id]
+  security_groups    = [aws_security_group.load_balancer.id]
   subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   enable_deletion_protection = true
--- calendar/infrastructure/security.tf
+++ calendar/infrastructure/security.tf
@@ -17,10 +17,6 @@ resource "aws_security_group" "allow_all" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags   = merge(local.tags, {
-    Name = "all-web-traffic"
-  })
 }
 
 resource "aws_security_group" "web_traffic" {
@@ -35,6 +31,7 @@ resource "aws_security_group" "web_traffic" {
     description = "Allow inbound HTTP traffic from anywhere"
   }
 
+  # todo, only allow traffic from LB
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,4 +43,33 @@ resource "aws_security_group" "web_traffic" {
   tags   = merge(local.tags, {
     Name = "ecs-web-traffic"
   })
+}
+
+resource "aws_security_group" "load_balancer" {
+  name   = "load_balancer"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow inbound HTTP traffic from anywhere"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow inbound HTTPS traffic from anywhere"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
 }
