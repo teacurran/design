3b7863ebb88ff7fc747da480a4c7cb8585fa1f00
Terrence Curran <tcurran@approachingpi.com>
2024-03-02 12:34:30 -0500

    moved API into environment variable. set things up for linting.

--- calendar/client/src/constants.ts
+++ calendar/client/src/constants.ts
@@ -1,7 +1,7 @@
 const { REACT_APP_API_URL } = process.env
 
-const API_URL = REACT_APP_API_URL || 'http://localhost:3400'
+const API_URL = REACT_APP_API_URL || "http://localhost:3400"
 
 export {
-  API_URL
+    API_URL
 }
