043bfc04cf044cfed753184cb634f4fe28b4e072
Terrence Curran <tcurran@approachingpi.com>
2024-03-03 10:45:19 -0500

    lint fixes

--- calendar/server/src/index.ts
+++ calendar/server/src/index.ts
@@ -2,7 +2,6 @@ import express, { type Express, type Request, type Response } from 'express'
 import dotenv from 'dotenv'
 import compression from 'compression'
 import * as d3 from 'd3'
-import { geoOrthographic } from 'd3-geo'
 import { JSDOM } from 'jsdom'
 import * as emoji from 'node-emoji'
 import * as fluent from 'fluentui-emoji-js'
@@ -112,107 +111,106 @@ export interface CalendarRequest extends ValidatedRequestSchema {
 
 app.get('/calendar',
   validator.query(calendarParams),
-  async (req: ValidatedRequest<CalendarRequest>, res: Response): Promise<void> => {
-    const calendar: Calendar = new Calendar()
-    // calendar.optVermontWeekends = true
-    calendar.optShowMoonPhase = req.query.showMoonPhases
-    calendar.optVermontWeekends = req.query.vermontWeekends
-    calendar.optShowGrid = req.query.showGrid
-    calendar.gridStroke = 'black'
-    calendar.yearFill = 'black'
-    calendar.yearX = 1400
-    calendar.rotateMonthNames = req.query.rotateMonthNames
-    calendar.monthNameFill = 'black'
-    calendar.optShowDayNames = req.query.showDayNames
-    calendar.hideWeekendDayNames = req.query.hideWeekendDayNames
-    calendar.optShowMoonIllumination = req.query.showMoonIllunination
+  async (req: ValidatedRequest<CalendarRequest>, res: Response) => {
+    try {
+      const calendar: Calendar = new Calendar()
+      // calendar.optVermontWeekends = true
+      calendar.optShowMoonPhase = req.query.showMoonPhases
+      calendar.optVermontWeekends = req.query.vermontWeekends
+      calendar.optShowGrid = req.query.showGrid
+      calendar.gridStroke = 'black'
+      calendar.yearFill = 'black'
+      calendar.yearX = 1400
+      calendar.rotateMonthNames = req.query.rotateMonthNames
+      calendar.monthNameFill = 'black'
+      calendar.optShowDayNames = req.query.showDayNames
+      calendar.hideWeekendDayNames = req.query.hideWeekendDayNames
+      calendar.optShowMoonIllumination = req.query.showMoonIllunination
 
-    const theme = req.query.theme
-    if (theme === 'vermontWeekends') {
-      calendar.optVermontWeekends = true
-    } else if (theme === 'rainbowWeekends') {
-      calendar.optRainbowWeekends = true
-    } else if (theme === 'rainbowDays1') {
-      calendar.optRainbowDays1 = true
-    } else if (theme === 'rainbowDays2') {
-      calendar.optRainbowDays2 = true
-    } else if (theme === 'rainbowDays3') {
-      calendar.optRainbowDays3 = true
-    }
+      const theme = req.query.theme
+      if (theme === 'vermontWeekends') {
+        calendar.optVermontWeekends = true
+      } else if (theme === 'rainbowWeekends') {
+        calendar.optRainbowWeekends = true
+      } else if (theme === 'rainbowDays1') {
+        calendar.optRainbowDays1 = true
+      } else if (theme === 'rainbowDays2') {
+        calendar.optRainbowDays2 = true
+      } else if (theme === 'rainbowDays3') {
+        calendar.optRainbowDays3 = true
+      }
 
-    const svgDom: d3.Selection<HTMLElement, unknown, null, undefined> = calendar.getSvgAsDocumentDom()
-    let svg = svgDom.html()
+      const svgDom: d3.Selection<HTMLElement, unknown, null, undefined> = calendar.getSvgAsDocumentDom()
+      let svg = svgDom.html()
 
-    let browser: puppeteer.Browser
-    let page: puppeteer.Page | undefined
-    if (req.query.optimize) {
-      const result = optimize(svg, {
-        multipass: true,
-        plugins: [
-          {
-            name: 'preset-default',
-            params: {
-              overrides: {
-                removeViewBox: false
+      let browser: puppeteer.Browser
+      let page: puppeteer.Page | undefined
+      if (req.query.optimize) {
+        const result = optimize(svg, {
+          multipass: true,
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false
+                }
               }
             }
-          }
-        ]
-      })
-      svg = result.data
+          ]
+        })
+        svg = result.data
+      }
+
+      if (req.query.format === 'pdf' || req.query.format === 'png') {
+        browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+        page = await browser.newPage()
+        await page.setContent(svg)
+      }
+
+      if (req.query.format === 'pdf' && page !== undefined) {
+        const pdf = await page.pdf({ format: 'A1', landscape: true, scale: 2 })
+
+        // set the filename with todays date
+        const filename = `calendar-${new Date().toISOString().split('T')[0]}.pdf`
+
+        res.contentType('application/pdf')
+        res.setHeader(
+          'Content-Disposition',
+          `attachment; filename=${filename}`
+        )
+
+        res.send(pdf)
+        return
+      }
+
+      if (req.query.format === 'png' && page !== undefined) {
+        const png = await page.screenshot({
+          type: 'png',
+          fullPage: true
+        })
+
+        // set the filename with todays date
+        const filename = `calendar-${new Date().toISOString().split('T')[0]}.png`
+
+        res.contentType('image/png')
+        res.setHeader(
+          'Content-Disposition',
+          `attachment; filename=${filename}`
+        )
+
+        res.send(png)
+        return
+      }
+      res.setHeader('Content-Type', 'image/svg+xml')
+      res.send(svg)
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('An error occurred while generating the PDF.')
     }
-
-    if (req.query.format === 'pdf' || req.query.format === 'png') {
-      browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-      page = await browser.newPage()
-      await page.setContent(svg)
-    }
-
-    if (req.query.format === 'pdf' && page !== undefined) {
-      const pdf = await page.pdf({ format: 'A1', landscape: true, scale: 2 })
-
-      // set the filename with todays date
-      const filename = `calendar-${new Date().toISOString().split('T')[0]}.pdf`
-
-      res.contentType('application/pdf')
-      res.setHeader(
-        'Content-Disposition',
-        `attachment; filename=${filename}`
-      )
-
-      res.send(pdf)
-      return
-    }
-
-    if (req.query.format === 'png' && page !== undefined) {
-      const png = await page.screenshot({
-        type: 'png',
-        fullPage: true
-      })
-
-      // set the filename with todays date
-      const filename = `calendar-${new Date().toISOString().split('T')[0]}.png`
-
-      res.contentType('image/png')
-      res.setHeader(
-        'Content-Disposition',
-        `attachment; filename=${filename}`
-      )
-
-      res.send(png)
-      return
-    }
-
-    res.setHeader('Content-Type', 'image/svg+xml')
-
-    res.send(svg)
   })
 
 app.get('/calendar2', async (req: Request, res: Response): Promise<void> => {
-  const geoProjection = geoOrthographic()
-    .translate([0, 0])
-    .scale(10)
-
   const width = gridWidth
   const height = gridHeight + 100
 
@@ -258,56 +256,56 @@ app.get('/calendar2', async (req: Request, res: Response): Promise<void> => {
 
         if (optUSFederalHolidays !== undefined) {
           const holiday = federalHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optCanadianHolidays !== undefined) {
           const holiday = canadianHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optOtherHolidays !== undefined) {
           const holiday = otherHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optCatholicHolidays !== undefined) {
           const holiday = catholicHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optJewishHolidays !== undefined) {
           const holiday = jewishHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optHinduHolidays !== undefined) {
           const holiday = hinduHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optIslamicHolidays !== undefined) {
           const holiday = islamicHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
 
         if (optChineseHolidays !== undefined) {
           const holiday = chineseHolidays.find(holiday => holiday.date.getTime() === date.getTime())
-          if (holiday) {
+          if (holiday !== undefined) {
             await appendEmoji(svg, holiday.emoji, x, y)
           }
         }
@@ -316,48 +314,8 @@ app.get('/calendar2', async (req: Request, res: Response): Promise<void> => {
   }
 
   res.setHeader('Content-Type', 'image/svg+xml')
-
   svg.attr('xmlns', 'http://www.w3.org/2000/svg')
-
   res.send(documentBody.html())
-})
-
-app.get('/moonmap', async (req: Request, res: Response): Promise<void> => {
-  const calendar: Calendar = new Calendar()
-  calendar.optShowMoonIllumination = true
-  calendar.optVermontWeekends = true
-  const svgDom = calendar.getSvgAsDocumentDom()
-  res.setHeader('Content-Type', 'image/svg+xml')
-  res.send(svgDom.html())
-})
-
-app.get('/moonmap.pdf', async (req: Request, res: Response): Promise<void> => {
-  const calendar: Calendar = new Calendar()
-  calendar.optRainbowWeekends = true
-  const svgDom = calendar.getSvgAsDocumentDom()
-
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
-  await page.setContent(svgDom.html())
-  const pdf = await page.pdf({ format: 'A1', landscape: true, scale: 2 })
-
-  res.contentType('application/pdf')
-  res.setHeader(
-    'Content-Disposition',
-    'attachment; filename=invoice.pdf'
-  )
-
-  res.send(pdf)
-})
-
-app.get('/daylight', async (req: Request, res: Response): Promise<void> => {
-  const calendar: Calendar = new Calendar()
-  calendar.optVermontWeekends = true
-  calendar.optShowMoonPhase = true
-  calendar.optShowGrid = true
-  const svgDom: d3.Selection<HTMLElement, unknown, null, undefined> = calendar.getSvgAsDocumentDom()
-  res.setHeader('Content-Type', 'image/svg+xml')
-  res.send(svgDom.html())
 })
 
 app.listen(port, () => {
@@ -380,5 +338,4 @@ app.get('/status', (req: Request, res: Response) => {
 
     res.send(revision)
   }
-}
-)
+})
