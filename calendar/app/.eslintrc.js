dd51bd86baaf3211b036ae21d2250f4f5c601802
Terrence Curran <tcurran@approachingpi.com>
2024-04-06 21:09:13 -0400

    eslint settings

--- calendar/app/.eslintrc.js
+++ calendar/app/.eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
         "@typescript-eslint/no-unsafe-call": "off",
         "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/require-await": "off"
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/return-await": "off"
       }
     }
   ],
