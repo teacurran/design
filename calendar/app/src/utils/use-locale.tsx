c4b5101eaae48a3f86816f700a061549a18a01e3
Terrence Curran <tcurran@approachingpi.com>
2024-08-04 23:58:34 -0400

    docker changes

--- calendar/app/src/utils/use-locale.tsx
+++
@@ -1,6 +0,0 @@
-import { useTranslation } from 'next-i18next'
-
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-export const useLocale = (
-  namespace: Parameters<typeof useTranslation>[0] = 'common'
-) => useTranslation(namespace)
